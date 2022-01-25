2c09fb273c967248ff28dd1cb8e9e845669b4524
Samuel Bryan <96742270+Samuelbryann@users.noreply.github.com>
2022-01-24 23:15:00 -0300

    Add files via upload

---
+++ vendas/one-page.js
@@ -0,0 +1,16 @@
+
+ function testSp()
+ {
+
+    const blocknome = document.getElementById("name")
+    var nome = prompt("Por favor digite seu primeiro nome!");
+    blocknome.innerHTML= `${nome}`
+
+ }
+ window.onload=testSp;
+
+
+
+
+
+
